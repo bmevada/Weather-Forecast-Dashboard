6c0fad5a655cc6457f4b977c0f52c1af3f9f7a4a
Bhavika Mevada <bmevada>
2022-09-20 21:50:07 +1000

    tidy js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ var todayContainer = document.querySelector('#today');
 var humidityEl;
 var humidity;
 var cityHistory = [];
+var clearhistoryBtn = document.querySelector("#clearHistory")
 
 var historyContainer = document.querySelector("#history")
 var forecastContainer = document.getElementById("forecast");
@@ -300,123 +301,13 @@ historyContainer.addEventListener("click", function (e) {
     displayHistory();
   }
 
-
-// // for loop- 5 cards to put the data to - function: card which will go through 5 times
-// function getForecastWeather(city) {
-//     console.log('inside function' + city);
-//     //Get data from OpenWeather URL using API Key: 
-//     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=531a6a316996024b38400dc9373b220d")
-//         .then((response) => response.json())
-//         // .then(function (response)
-//         .then((data) => {
-
-//             console.log(data);
-
-//             // Display weather forecast for today
-//             //todayweatherEl.classList.remove("d-none");
-
-//             // Parse response to display current weather
-//             for (let i = 0; i <= 5; i++) {
-//                 // const currentDate = new Date(data.list[i].dt * 1000);
-//                 // const day = currentDate.getDate();
-//                 // const month = currentDate.getMonth() + 1;
-//                 // const year = currentDate.getFullYear();
-//                 // let currentDateEl = document.querySelector("#day-one-date-day" + i);
-//                 // currentDateEl.innerHTML = data.city.name + " (" + day + "/" + month + "/" + year + ") ";;
-//                 // let weatherPic = data.list[i].weather[0].icon;
-//                 // let currentPicEl = document.querySelector("#day" + i);
-//                 // currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
-//                 // currentPicEl.setAttribute("alt", data.list[i].weather[0].description);
-//                 // let currentTempEl = document.querySelector("#day-one-temperature-day" + i);
-//                 // //Convert from F to C
-//                 // currentTempEl.innerHTML = "Temperature: " + data.list[i].main.temp + " &#176F";
-//                 // let currentHumidityEl = document.querySelector("#day-one-humidity-day" + i);
-//                 // currentHumidityEl.innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
-//                 // let currentWindEl = document.querySelector("#day-one-wind-day" + i);
-//                 // currentWindEl.innerHTML = "Wind Speed: " + data.list[i].wind.speed + " MPH";
-//             }
-
-
-//         })
-//     //TODO: save local storage in an multimensional array where city and weather data are linked.
-//     if (city == null) {
-//         city = [];
-//         city.push(city.searchHistory()
-//         );
-//         localStorage.setItem("cityname", JSON.stringify(city));
-//         addToList(city);
-//     }
-//     else {
-//         if (find(city) > 0) {
-//             city.push(city.searchHistory());
-//             localStorage.setItem("cityname", JSON.stringify(city));
-//             addToList(city);
-//         }
-//     }
-
-// }
-
-// // view recently searched cities
-// function loadlastCity() {
-//     $("ul").empty();
-//     var city = JSON.parse(localStorage.getItem("cityname"));
-//     if (city !== null) {
-//         city = JSON.parse(localStorage.getItem("cityname"));
-//         for (i = 0; i.city.length; i++) {
-//             addToList(city[i]);
-//         }
-//         city = city[i - 1];
-//         currentWeather(city);
-//     }
-
-// }
-
-// // Store data in local storage
-// function checkLocalStorage() {
-//     var storedData = localStorage.getItem('queries');
-//     var dataArray = [];
-//     if (!storedData) {
-//         console.log("no data stored");
-//     } else {
-//         storedData.trim();
-//         dataArray = storedData.split(',');
-//         for (var i = 0; i < dataArray.length; i++) {
-//             createRecentSearchBtn(dataArray[i]);
-//         }
-//     }
-// };
+//Clear search history
+function clearHistory (event) {
+    event.preventDefault();
+    cityName = [];
+    localStorage.removeItem("cityName");
+    document.location.reload();
+}
 
 
 
-// // Function to Set data in Local storage
-// // function saveToLocalStorage(q) {
-// //     var data = localStorage.getItem('queries');
-// //     if (data) {
-// //         console.log(data, q)
-
-// //     } else {
-// //         data = q;
-// //         localStorage.setItem('queries', data);
-// //     }
-// //     if (data.indexOf(q) === -1) {
-// //         data = data + ',' + q;
-// //         localStorage.setItem('queries', data);
-// //         createRecentSearchBtn(q);
-// //     }
-// // }
-
-// // Retreive data from local storage
-// // searchEl.addEventListener("click", function () {
-// //     const searchTerm = cityEl.value;
-// //     getWeather(searchTerm);
-// //     searchHistory.push(searchTerm);
-// //     localStorage.setItem("search", JSON.stringify(searchHistory));
-// //     renderSearchHistory();
-// // })
-
-// // Clear search history
-// clearEl.addEventListener("click", function () {
-//     localStorage.clear();
-//     searchHistory = [];
-//     renderSearchHistory();
-// })
